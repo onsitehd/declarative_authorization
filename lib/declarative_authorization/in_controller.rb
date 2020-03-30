303be24815c4df042f9b1655e5e5b1ecbe34362f
Victor Adeolu <vadeolu@onsitehd.com>
2020-03-30 12:03:31 -0500

    Fix recursive loop in `#filter_access_permissions` method

--- lib/declarative_authorization/in_controller.rb
+++ lib/declarative_authorization/in_controller.rb
@@ -602,7 +602,7 @@ module Authorization
 
       def filter_access_permissions # :nodoc:
         unless filter_access_permissions?
-          ancestors[1..-1].reverse.each do |mod|
+          (ancestors - [self]).reverse_each do |mod|
             mod.filter_access_permissions if mod.respond_to?(:filter_access_permissions, true)
           end
         end
